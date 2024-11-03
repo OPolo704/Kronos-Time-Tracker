34abbb4893f2035b765738e7312b97496b21bbd4
OPolo704 <GeoDasher704@gmail.com>
2024-11-03 16:16:35 +0100

    category manager & manager list style, functionality

--- stats.js
+++ stats.js
@@ -1,3 +1,32 @@
 categoryData = JSON.parse(sessionStorage.getItem("categoryData"));
 sessionData = JSON.parse(sessionStorage.getItem("sessionData"));
 
+// CATEGORY MANAGER
+
+const categoriesbtn = document.querySelector(".categories-btn");
+const categoryManager = document.querySelector(".category-manager");
+const categoryManagerList = document.querySelector(".category-manager-list");
+
+createCategoryManagerList();
+
+function createCategoryManagerList() {
+  categoryData.forEach((cat) => {
+    const catElement = document.createElement("li");
+    catElement.textContent = cat.name;
+
+    categoryManagerList.appendChild(catElement);
+  });
+}
+
+categoriesbtn.onclick = () => {
+  categoryManager.classList.remove("hidden");
+};
+
+const toggler = document.querySelectorAll(".caret");
+
+for (let i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function () {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
