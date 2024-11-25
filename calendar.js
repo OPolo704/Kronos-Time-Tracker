ac5d7209b89f6e488954b032e2fedfbe9cf6dad7
OPolo704 <GeoDasher704@gmail.com>
2024-11-25 23:27:48 +0100

    date select lists added and functional

--- calendar.js
+++ calendar.js
@@ -1,7 +1,12 @@
+const yearbtn = document.querySelector(".year-btn");
+const monthbtn = document.querySelector(".month-btn");
+const daybtn = document.querySelector(".day-btn");
+
 let chronologicalData = [];
+let currentDate = [2024, 9, 11];
 
 sessionData.forEach((catID, index) => {
-  // for each catID find the corresponding category to the id in categoryData and grab the name and color
+  // insert sessions to the chronologicalData array
   const cat = categoryData.find((cat) => cat.id === index);
 
   catID.forEach((session) => {
@@ -23,6 +28,8 @@ sessionData.forEach((catID, index) => {
   });
 });
 
+createDateList();
+
 function createYear(year) {
   yearSessions = [];
   for (i = 0; i < 12; i++) {
@@ -39,3 +46,74 @@ function createYear(year) {
   };
   chronologicalData.push(yearObject);
 }
+
+function createDateList() {
+  const today = new Date();
+
+  currentDate = [
+    today.getFullYear(),
+    today.getMonth() + 1,
+    today.getDate() + 1,
+  ];
+
+  yearbtn.textContent = currentDate[0];
+  monthbtn.textContent = currentDate[1];
+  daybtn.textContent = currentDate[2];
+
+  chronologicalData.forEach((year) => {
+    const newbtn = document.createElement("button");
+    newbtn.textContent = year.year;
+    newbtn.onclick = (event) => {
+      currentDate[0] = +event.target.textContent;
+      yearbtn.textContent = currentDate[0];
+      event.target.parentElement.classList.add("hidden");
+    };
+
+    const yearList = document
+      .querySelector(".year")
+      .querySelector(".date-list");
+    yearList.prepend(newbtn);
+  });
+
+  const monthList = document
+    .querySelector(".month")
+    .querySelector(".date-list");
+  const dayList = document.querySelector(".day").querySelector(".date-list");
+  for (let i = 0; i < 12; i++) {
+    const newbtn = document.createElement("button");
+    newbtn.textContent = i + 1;
+    newbtn.onclick = (event) => {
+      currentDate[1] = +event.target.textContent;
+      monthbtn.textContent = currentDate[1];
+      event.target.parentElement.classList.add("hidden");
+    };
+
+    monthList.append(newbtn);
+  }
+
+  for (let i = 0; i < 31; i++) {
+    const newbtn = document.createElement("button");
+    newbtn.textContent = i + 1;
+    newbtn.onclick = (event) => {
+      currentDate[2] = +event.target.textContent;
+      daybtn.textContent = currentDate[2];
+      event.target.parentElement.classList.add("hidden");
+    };
+
+    dayList.append(newbtn);
+  }
+}
+
+function printDay() {}
+
+yearbtn.onclick = (event) => {
+  event.currentTarget.nextElementSibling.classList.toggle("hidden");
+};
+
+monthbtn.onclick = (event) => {
+  event.currentTarget.nextElementSibling.classList.toggle("hidden");
+};
+
+daybtn.onclick = (event) => {
+  event.currentTarget.nextElementSibling.classList.toggle("hidden");
+};
